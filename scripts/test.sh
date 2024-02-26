db071166df7d4143699f175913ac022a4bd7c4d5
Balázs Kiss <dev@balazskiss.name>
2024-02-26 14:49:44 +0100

    small updates to pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 XCODE_PROJECT_PATH="${PROJECT_DIR}/KaraokeApp.xcodeproj"
 SCHEME_NAME="KaraokeApp"
-DESTINATION="platform=iOS Simulator,name=iPhone 15,OS=17.0"
+DESTINATION="platform=iOS Simulator,name=iPhone 15,OS=17.2"
 
 xcodebuild test \
   -project "$XCODE_PROJECT_PATH" \
