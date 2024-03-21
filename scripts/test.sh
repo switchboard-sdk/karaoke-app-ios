333a9434dbc1ecabe10fe4910a38f37ea3b15e45
Balázs Kiss <dev@balazskiss.name>
2024-03-21 10:46:23 +0100

    Updated test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,9 +5,10 @@ set -eu
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 XCODE_PROJECT_PATH="${PROJECT_DIR}/KaraokeApp.xcodeproj"
 SCHEME_NAME="KaraokeApp"
-DESTINATION="platform=iOS Simulator,name=iPhone 15,OS=17.2"
+DESTINATION="platform=iOS Simulator,name=iPhone 15"
 
-xcodebuild test \
+xcodebuild clean test \
   -project "$XCODE_PROJECT_PATH" \
   -scheme "$SCHEME_NAME" \
+  -derivedDataPath "${PROJECT_DIR}/build/XcodeDerivedData" \
   -destination "$DESTINATION"
